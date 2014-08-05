1958e5fd107fa577576200ce225ee27b945347e4
Hack Reactor Students <Jonsadka@gmail.com>
2014-08-04 21:36:38 -0700

    Add submit message functionality & worked on front end

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -8,9 +8,23 @@ var app = {};
 
 // init checks data for comprimised messages and returns the non-comprimised ones
 app.init = function(dataFromServer){
-  // create a test to filter comprimised messages
-
-  return dataFromServer;
+  // create a test to filter out comprimised messages contained within any message property
+  var reducedData = [];
+  for( var i = 0, count = dataFromServer.results.length; i < count; i++ ){
+    var safe = true;
+    for( key in  dataFromServer.results[i] ){
+      if ( /((\%3C)|<)((\%2F)|\/)*[a-z0-9\%]+((\%3E)|>)/.test(dataFromServer.results[i][key]) ||
+           /((\%3C)|<)((\%69)|i|(\%49))((\%6D)|m|(\%4D))((\%67)|g|(\%47))[^\n]+((\%3E)|>)/.test(dataFromServer.results[i][key]) ||
+           /((\%3C)|<)[^\n]+((\%3E)|>)/.test(dataFromServer.results[i][key]) ||
+           !/[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$/.test(dataFromServer.results[i][key])){
+           safe = false;
+      }
+    }
+    // if all message properties are safe, save the message and send it to the user
+    if(safe) reducedData.push( dataFromServer.results[i] );
+  }
+  var filteredData = {results: reducedData};
+  return filteredData;
 };
 
 // send takes in a message object containing username, text, and roomname and ???TBD??
@@ -34,15 +48,13 @@ app.fetch = function( urlLocation ){
   $.ajax({
     url: urlLocation || 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
-    // data: JSON.stringify(data),
+    data: {order: '-createdAt'},
     contentType: 'application/json',
     success: function ( serverMessages ) {
-      // return safe messages
+      // filter through each message and only return safe messages
       var filteredServerMessages = app.init( serverMessages );
-
-      // continue to append each message to the DOM
-      each(filteredServerMessages, app.addMessage( singleFilteredMessage ));
-      console.log('chatterbox: Sucessfully retrieved messages from server.');
+      _.each(filteredServerMessages.results, app.addMessage);
+      console.log('chatterbox: Sucessfully retrieved safe messages from server.');
     },
     error: function (data) {
       console.error('chatterbox: Failed to retrieve messages from server.');
@@ -53,7 +65,7 @@ app.fetch = function( urlLocation ){
 // clear all messages in browser
 app.clearMessages = function() {
   $('#chats').children().remove();
-}
+};
 
 // display all outstanding messages to user
 app.addMessage = function( messageToDisplay ) {
@@ -61,10 +73,11 @@ app.addMessage = function( messageToDisplay ) {
   var roomName =  '<div class="roomname">' + messageToDisplay.roomname + '</div>';
   var text =  '<div class="text">' + messageToDisplay.text + '</div>';
 
-  var newMessage = '<div>' + userName + ' in ' +
-                             roomName + ': ' +
-                             text + '</div>';
-  $('#chats').prepend( newMessage );
+  var newMessage = '<div class="fullmessage">' + messageToDisplay.createdAt +
+                                                 userName + ' in ' +
+                                                 roomName + ': ' +
+                                                 text + '</div>';
+  $('#chats').append( newMessage );
 };
 
 // add a chat room
@@ -78,17 +91,19 @@ app.addFriend = function( userNameNode ) {
   friends[ userNameNode ] = userNameNode; //C NEED TO REFORMAT
 };
 
-app.handleSubmit = function( textToSend ){
+app.handleSubmit = function( userNameToSend, textToSend, roomToSend ){
   // compile message object with username, text, and roomname
-  var messageToServer = { username: undefined,
+  var messageToServer = { username: userNameToSend,
                           text: textToSend,
-                          roomname: undefined };
+                          roomname: roomToSend };
 
   // send the message object
   app.send( messageToServer );
 };
 
 $(document).ready( function(){
+
+
   // once data has been retrieved, add those messages
   app.fetch( url );
 
@@ -98,10 +113,16 @@ $(document).ready( function(){
   });
 
   // extract text and pass to handleSubmit to format before sending to server
-  $('#send .submit').on('submit', function(){
+  $('#send').on('click', 'button',function(){
+    var username = $('#currentuser').val();
     var text = $('#message').val();
-    app.handleSubmit( text );
+    app.handleSubmit( username, text, "room" );
   });
+
+  setInterval( function(){
+    app.clearMessages();
+    app.fetch(url);
+  }, 3000 );
 
   // have a listener to clear all messages using app.clearMessages
 
